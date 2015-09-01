f49d9884c5711f8594dd9d3e9bd5e24a804893e6
yrsegal <yrsegal@gmail.com>
2015-09-01 16:25:28 -0400

    duplicates now removed by sortwords.py

--- sortwords.py
+++ sortwords.py
@@ -5,5 +5,10 @@ wordpath = os.path.join(path, "defaultwords.json")
 if __name__ == "__main__":
 	words = json.load(open(wordpath))
 	for i in words:
-		words[i].sort()
+		nwordsi = []
+		for j in words[i]:
+			if j not in nwordsi:
+				nwordsi.append(j)
+		nwordsi.sort()
+		words[i] = nwordsi
 	json.dump(words, open(wordpath, "w"), indent=2, sort_keys=True)
